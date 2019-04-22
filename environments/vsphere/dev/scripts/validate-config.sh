d0cdeda464f0f9677b3775067656304e837451d6
Caleb Washburn <cwashburn@pivotal.io>
2019-04-22 15:47:17 -0700

    update to add new logic

---
+++ environments/vsphere/dev/scripts/validate-config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+if [ $# -eq 0 ]; then
+  echo "Must supply product name as arg"
+  exit 1
+fi
+
+product=$1
+echo "Validating configuration for product $product"
+
+touch ../config/vars/${product}.yml
+touch expected_secrets/${product}.yml
+
+bosh int --var-errs --var-errs-unused ../config/templates/${product}.yml --vars-file ../config/defaults/${product}.yml --vars-file ../config/vars/${product}.yml --vars-file expected_secrets/${product}.yml
